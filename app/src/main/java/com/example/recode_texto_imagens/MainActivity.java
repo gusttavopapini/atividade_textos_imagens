cf94f72110b20e543030020124857bc083881dec
Gustavo Papini <gusttavopapini@gmail.com>
2021-04-21 10:38:16 -0300

    subindo repositório

---
+++ app/src/main/java/com/example/recode_texto_imagens/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recode_texto_imagens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
